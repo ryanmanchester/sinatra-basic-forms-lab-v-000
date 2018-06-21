3894fd78a7cd5419a16c1306b2beac4286839166
Ryan Manchester <ryanm17@gmail.com>
2018-06-21 15:56:29 +0000

    automatically backed up by learn

--- spec/sinatra_basic_forms_lab_spec.rb
+++ spec/sinatra_basic_forms_lab_spec.rb
@@ -35,7 +35,7 @@ describe App do
       fill_in(:name, :with => "Butch")
       fill_in(:breed, :with => "Mastiff")
       fill_in(:age, :with => "6 months")
-      # click_button "submit"
+       click_button "submit"
       expect(page).to have_text("Puppy Name:\nButch")
       expect(page).to have_text("Puppy Breed:\nMastiff")
       expect(page).to have_text("Puppy Age:\n6 months")
